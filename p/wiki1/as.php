1b4279627589d838feb250dd8f4c07ae58a56e67
Puneet Aggarwal <9988182547p@gmail.com>
2016-07-01 11:38:07 +0530

    test file

---
+++ p/wiki1/as.php
@@ -0,0 +1,19 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.min.css">
+<link rel="stylesheet" type="text/css" href="css/home.css">
+
+<html>
+<head>
+<title>Wikipedia Analyser</title>
+</head>
+<body>
+<div class="container">
+<input type="text" id="w">
+<button class="btn btn-primary">yoyo</button>
+</div>
+<body>
+
+</html>
