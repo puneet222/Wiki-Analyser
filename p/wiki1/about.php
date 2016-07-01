c0015341acb00bc5fc401e88be3bd2d932f1a6c7
Puneet Aggarwal <9988182547p@gmail.com>
2016-07-01 11:09:22 +0530

    about the project

---
+++ p/wiki1/about.php
@@ -0,0 +1,45 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.min.css"> -->
+<link rel="stylesheet" type="text/css" href="css/home.css">
+<link rel="stylesheet" type="text/css" href="font-awesome-4.6.3/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="font-awesome-4.6.3/css/font-awesome.css">
+
+
+
+<html>
+<head>
+<title>Wikipedia Analyser</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
+<script src="javaS/first.js"></script>
+<style type="text/css">
+	body{
+		background-color: #EF5350;
+	}
+</style>
+</head>
+<body>
+
+<div id="nav">
+<ul>
+  <li id="wikianly"><a href="#"><h3>Wikipedia Analyser</h3></a></li>
+  <li id="help"><a href="help.php"><h4>Help</h4></a></li>
+  <li id="contact"><a href="contact.php"><h4>Contact</h4></a></li>
+  <li id="about"><a href="about.php"  class="active"><h4>About</h4></a></li>
+</ul>
+</div>
+<br>
+<br>
+<br>
+<div class="container">
+
+<br><br><br><br>
+<h3><p style="color:white">Wikipedia Analyser is a web application developed by <a href="#">Puneet Aggarwal</a>.This application is a Wikipedia viewer which also analyses your searched topic.There are two types os Analysation :
+(i) Basic Data Analysation
+(2) Page view Analysation</p></h3>
+
+</div>
+</body>
+</html>
